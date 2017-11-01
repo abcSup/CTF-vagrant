1c83461b3bd3105aeee571782bb5c4ae682b4a6e
abcSup <kristan97@hotmail.com>
2017-11-01 16:35:12 -0700

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,8 @@
+# vim: set filetype=ruby :
+
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.provision :shell, path: "bootstrap.sh", privileged: false
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y git build-essential
+sudo apt-get install -y gdb gdb-multiarch
+sudo apt-get install -y tmux
+
+# python
+sudo apt-get -y install python2.7 python-pip python-dev git libssl-dev libffi-dev
+sudo apt-get -y install python3-pip
+
+# 32-bit support
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
+
+# pwntools
+sudo pip install --upgrade pip
+sudo pip install --upgrade pwntools
+
+# pwndbg
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+cd
+
+# r2
+git clone https://github.com/radare/radare2
+cd radare2
+./sys/install.sh
+cd
+
+# binwalk
+git clone https://github.com/devttys0/binwalk
+cd binwalk
+sudo python3 setup.py install
+cd
+
